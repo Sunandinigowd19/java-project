6ad4bc461a62d0bff249876f8a9aa23f7ff2033f
Sunandinigowd19 <gowdsunandini@gmail.com>
2025-06-09 21:21:53 +0530

    Add files via upload

---
+++ Abstract.java
@@ -0,0 +1,25 @@
+package src;
+abstract class Examp{
+	abstract void car();
+}
+class AC extends Examp{
+	void car() {
+		System.out.println("license");
+	}
+}
+class BC extends Examp{
+	void car() {
+		System.out.println("Driving");
+	}
+}
+public class Abstract {
+
+	public static void main(String[] args) {
+           Examp obj=new BC();
+           Examp objj=new AC();
+		obj.car();
+		objj.car();
+	}
+
+}
+
